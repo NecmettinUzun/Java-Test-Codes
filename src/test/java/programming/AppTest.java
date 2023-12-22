cf7f80ac9e736e1e5be4773f52e3fb1f50c187a5
ARGELA\necmettinuz <necmettin.uzun@argela.com.tr>
2023-12-22 10:16:39 +0300

    package updated

---
+++ src/test/java/programming/AppTest.java
@@ -0,0 +1,14 @@
+package programming;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
